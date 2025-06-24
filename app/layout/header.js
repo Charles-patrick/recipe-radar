2ed8465f4cb314ebb21ddb88ddf4d8a3c2d6ccf8
Charles-patrick <ebukacharles006@gmail.com>
2025-06-24 17:09:30 +0100

    second commit

--- app/layout/header.js
+++ app/layout/header.js
@@ -1,19 +1,22 @@
 import Link from 'next/link'
-import { LuRadar } from'react-icons/lu'
+import { LuRadar } from 'react-icons/lu'
+
 export const Header = () => {
-    return(
-        <>
-         <section className="flex justify-between align-center px-5 py-3 bg-black text-white fixed top-0 w-full z-50 max-w-[1200px]">
-            <div >
-                <Link href='/' className='flex justify-between items-center gap-1'>
-                    <LuRadar /> <h1>Recipe Radar</h1>
+    return (
+        <header className="fixed top-0 left-0 w-full z-50 bg-black text-white">
+            <div className="flex justify-between items-center px-5 py-3 max-w-[1200px] mx-auto">
+                <Link href='/' className='flex items-center gap-2'>
+                    <LuRadar size={24} />
+                    <h1 className="text-lg font-bold">Recipe Radar</h1>
                 </Link>
+                <nav>
+                    <ul className="flex gap-4">
+                        <li>
+                            <Link href='#' className="hover:underline">Favorites</Link>
+                        </li>
+                    </ul>
+                </nav>
             </div>
-            <div>
-                <ul> <li> <Link href='#'>Favorites</Link> </li> </ul>
-            </div>
-         </section>
-        </>
-        
+        </header>
     )
 }
